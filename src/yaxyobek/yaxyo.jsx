e42371c6a2863a98077c84b221b3cebc6c630fe7
yaxyobek2010yil-ui <yaxyobek2010yil@gmail.com>
2026-07-05 21:27:54 +0500

    tugatdim

--- src/yaxyobek/yaxyo.jsx
+++ src/yaxyobek/yaxyo.jsx
@@ -1,22 +1,16 @@
 import React from 'react'
 import "./yaxyo.css"
+import zor from "../zor.png"
 const Yaxyo = () => {
   return (
-    <div className='sayt'>Yaxyobek
-    
-    
-    
-    <h1>salom</h1>
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    <div className='sayt'>
+    <div className="box">
+      <h2>the gear cage</h2>
+      <h1>The tools that we use</h1>
+      <h5>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro consequuntur nisi exercitationem vel, totam, fugiat ex pariatur optio in voluptate nihil odio excepturi repellendus ipsam provident non nam a fuga consequatur consectetur.</h5>
+      <button><div className="b-ich"></div> <h6>Check it out</h6></button>
+    </div>
+    <div className="mox"></div>
     </div>
   )
 }
