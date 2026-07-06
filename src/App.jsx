1bab2f40cb6f0d1a96ee5e9be61e0040e3023032
ToxirjonovN65 <muhammadyunustoxirjonov77@gmail.com>
2026-07-06 17:57:00 +0500

    yana

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Cards from './New_Production/Cards/Cards'
 import Yaxyo from './yaxyobek/yaxyo'
 import Kamron from './Kamron/kamron'
+import Yunus from './muhammadyunus/yunus'
 
 
 const App = () => {
