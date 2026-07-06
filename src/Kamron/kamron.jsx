cd5401c37cf9c2628ba8f40ff1e9afbaf96245f9
khamidullayevaziko7-hue <aligangbot@gmail.com>
2026-07-06 17:12:49 +0500

    kamronbek files

---
+++ src/Kamron/kamron.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './kamron.css'
+
+const Kamron = () => {
+    return (
+        <div>
+            <div className="right">
+                <h1>Sunset at Mount Fuji</h1>
+                <h4>Duis tristique sed lorem a vestibulum. Cras commodo consequat orci, in convallis risus egestas non.</h4>
+            </div>
+        </div>
+    )
+}
+
+export default Kamron
