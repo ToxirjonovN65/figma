dae705313eb2342d681c75575e476b0ee58f7f8c
Kamrobek1774-Kali <kamron1774@gmail.com>
2026-07-06 17:53:36 +0500

    all files

--- src/New_Production/Cards/Cards.jsx
+++ src/New_Production/Cards/Cards.jsx
@@ -14,23 +14,24 @@ const Cards = () => {
             <div className="card_1">
               <h4>Product Photography</h4>
               <p>Cras commodo consequat orci, in convallis risus egestas non. Nulla efficitur auctor hendrerit. Etiam ut orci varius, faucibus libero ac, cursus quam.</p>
-              <button><a href="#">Read more</a></button>
+              <button className='b_1'><a href="#">Read more</a></button>
             </div>
             <div className="card_2">
               <h4>Architecture Photography</h4>
               <p>Aenean porta neque eget consequat fringilla. Vestibulum ultrices, orci nec egestas pharetra, ligula est semper enim, nec auctor sapien leo nec purus. Fusce tincidunt aliquet sapien, sit amet rhoncus leo imperdiet nec.</p>
-              <button><a href="#">Read more</a></button>
+              <button className='b_1'><a href="#">Read more</a></button>
             </div>
             <div className="card_3">
               <h4>Drone Photography</h4>
               <p>Mauris euismod elit et nisi ultrices, ut faucibus orci tincidunt. Duis tristique sed lorem a vestibulum. Cras commodo consequat orci, in convallis risus egestas non. Nulla efficitur auctor hendrerit. Etiam ut orci varius, faucibus libero ac, cursus quam.</p>
-              <button><a href="#">Read more</a></button>
+              <button className='b_1'><a href="#">Read more</a></button>
             </div>
             <div className="card_4">
               <h4>Wildlife Photography</h4>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam scelerisque aliquam odio et faucibus. Nulla rhoncus feugiat eros quis consectetur. Morbi neque ex, condimentum dapibus congue et, vulputate ut ligula. Vestibulum sit amet urna turpis.</p>
-              <butoon><a href="#">Read more</a></butoon>
+              <button className='b_1'><a href="#">Read more</a></button>
             </div>
+            
         </div>
     </div>
 
